c3359c553309e8a2a87da259c9207080a5e17ed8
PaulSzyller <paulszyller@gmail.com>
2016-07-18 18:01:59 -0500

    Fixed error message during migration

--- app/config/database.php
+++ app/config/database.php
@@ -54,7 +54,8 @@ return array(
 
 		'mysql' => array(
 			'driver'    => 'mysql',
-			'host'      => getenv('DBHOST'),
+			'host'      => '127.0.0.1',
+			'unix_socket' => '/Applications/MAMP/tmp/mysql/mysql.sock',
 			'database'  => getenv('DBNAME'),
 			'username'  => getenv('DBUSERNAME'),
 			'password'  => getenv('DBPASS'),
