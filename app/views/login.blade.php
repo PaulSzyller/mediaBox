e7bdb7f47265e4eef6c30656964b72d05a3025df
PaulSzyller <paulszyller@gmail.com>
2016-07-18 16:02:37 -0500

    Added master layout and basic login view

---
+++ app/views/login.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.master')
+
+@section('title')
+    MediaBox | Login Page
+@stop
+
+@section('style')
+@stop
+
+@section('content')
+    <div class="container">
+        <h1>Login Here</h1>
+    </div>
