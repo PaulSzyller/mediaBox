f98d0c33266f4c2b8bd5863c345a6219f0131711
PaulSzyller <paulszyller@gmail.com>
2016-07-21 17:21:36 -0500

    Library layout now extends master layout

--- app/views/layouts/library.blade.php
+++ app/views/layouts/library.blade.php
@@ -1,28 +1,14 @@
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+@extends('layouts.master')
 
-    <!-- Font Awesome -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.0/css/font-awesome.min.css">
-    <link href="https://fonts.googleapis.com/css?family=Titillium+Web" rel="stylesheet">
+@section('title')
+    @yield('title')
+@stop
 
-    {{HTML::style('css/bootstrap.css')}}
-    {{HTML::style('css/mdb.css')}}
-    {{HTML::style('css/colors.css')}}
-    {{HTML::style('css/fonts.css')}}
-    {{HTML::style('css/styles.css')}}
+@section('style')
+    @yield('style')
+@stop
 
-    <title>@yield('title')</title>
-    <style>@yield('style')</style>
-</head>
-<body>
-<header>
-        @include('widgets.navbar_loggedin')
-</header>
-
-<main>
+@section('content')
     <div class="container">
         <div class="row">
             <div class="col-xs-8">
@@ -58,20 +44,4 @@
         </div>
 
     </div>
-</main>
-
-<footer>
-    <!-- Footer goes here -->
-</footer>
-
-<!-- These files need to stay in this order -->
-{{HTML::script('js/jquery-2.2.3.js')}}
-{{HTML::script('js/tether.js')}}
-{{HTML::script('js/bootstrap.js')}}
-{{HTML::script('js/mdb.js')}}
-{{HTML::script('js/init.js')}}
-
-<!-- Custom JavaScript files go here -->
-
-</body>
-</html>
+@stop
