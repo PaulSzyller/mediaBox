015ce3a04ddfb1456c84411136d3dcfa72929480
PaulSzyller <paulszyller@gmail.com>
2016-07-31 13:46:03 -0500

    Re activated redirect away from dashboard when not logged in

--- app/controllers/DashboardController.php
+++ app/controllers/DashboardController.php
@@ -4,10 +4,10 @@ class DashboardController extends \BaseController {
 
     public function showDashboardView(){
 
-        /*if(Auth::check())
+        if(Auth::check())
             return View::redirect('dashboard');
 
-        return View::make('login');*/
+        return View::make('login');
 
         return View::make('dashboard');
     }
