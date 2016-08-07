f49a069f47302d88d3601808e71540fd8614030d
m-dabas <dabas1994@gmail.com>
2016-08-07 15:48:03 -0500

    cleaner profile display

--- app/views/profile.blade.php
+++ app/views/profile.blade.php
@@ -14,44 +14,34 @@
                 <div class="card card-block">
                     <h4 class="card-title">Username</h4>
                     <p>{{$user->username}}</p>
-                </div>
-                <div class="card card-block">
+                    <hr>
                     <h4 class="card-title">Email</h4>
                     <p>{{$user->email}}</p>
-                </div>
-                <div class="card card-block">
+                    <hr>
                     <h4 class="card-title">Password</h4>
                     <p>*****</p>
-                </div>
-                <div class="card card-block">
+                    <hr>
                     <h4 class="card-title">First Name</h4>
                     <p>{{$user->first_name}}</p>
-                </div>
-                <div class="card card-block">
+                    <hr>
                     <h4 class="card-title">Last Name</h4>
                     <p>{{$user->last_name}}</p>
-                </div>
-                <div class="card card-block">
+                    <hr>
                     <h4 class="card-title">Profile Picture</h4>
                     <p>{{$user->profile_pic}}</p>
-                </div>
-                <div class="card card-block">
+                    <hr>
                     <h4 class="card-title">Location</h4>
                     <p>{{$user->location}}</p>
-                </div>
-                <div class="card card-block">
+                    <hr>
                     <h4 class="card-title">Date of Birth</h4>
                     <p>{{$user->dob}}</p>
-                </div>
-                <div class="card card-block">
+                    <hr>
                     <h4 class="card-title">Security Question</h4>
                     <p>*****</p>
-                </div>
-                <div class="card card-block">
+                    <hr>
                     <h4 class="card-title">Security Answer</h4>
                     <p>*****</p>
-                </div>
-                <div class="card card-block">
+                    <hr>
                     <h4 class="card-title">Gender</h4>
                     <p>{{$user->gender}}</p>
                 </div>
